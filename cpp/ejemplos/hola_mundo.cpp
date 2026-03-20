2502c7a55c9a16e72c3b59c72851e0d1b8ed4b09
José Manuel Noriega Pineda <josemanuelnoriegapineda@gmail.com>
2026-03-19 21:19:24 -0600

    actualizando archivos tarea 3

--- cpp/ejemplos/hola_mundo.cpp
+++ cpp/ejemplos/hola_mundo.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main() {
   std::cout << "Hola C++ desde code-server\n";
-  std::cout << "<<Nombre>> y <<Carnet>>\n";
+  std::cout << "<<Jose Noriega>> y <<202505312>>\n";
   return 0;
 }
